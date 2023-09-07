daaff5d2942b6b707bf6e6e80d8c4d4887e284da
IagoMelo01 <iagomello160@gmail.com>
2023-09-06 22:21:23 -0300

    criando e instanciando a classe mensagem

--- processa_envio.php
+++ processa_envio.php
@@ -1,2 +1,40 @@
 <?php
-    print_r($_POST);
+    // print_r($_POST);
+
+    class Mensagem {
+        private $para = NULL;
+        private $assunto = null;
+        private $mensagem = null;
+
+        public function __get($atributo){
+            return $this->$atributo;
+        }
+
+        public function __set($atributo, $valor){
+            $this->$atributo = $valor;
+        }
+
+        public function mensagemValida(){
+            if(empty($this->para) || empty($this->assunto) || empty($this->mensagem)){
+                return false;
+            }
+
+            return true;
+        }
+
+        
+    }
+
+    $mensagem = new Mensagem();
+
+    $mensagem->__set('para',$_POST['para']);
+    $mensagem->__set('assunto',$_POST['assunto']);
+    $mensagem->__set('mensagem',$_POST['mensagem']);
+
+    // print_r($mensagem);
+
+    if($mensagem->mensagemValida()){
+        echo 'A mensagem é válida';
+    } else {
+        echo 'A mensagem não é válida';
+    }
