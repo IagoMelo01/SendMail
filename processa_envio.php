ccebb2b002db5cf491561d5a2e406912772936eb
IagoMelo01 <iagomello160@gmail.com>
2023-09-11 20:46:47 -0300

    Interface visula mais amigável

--- processa_envio.php
+++ processa_envio.php
@@ -16,6 +16,7 @@
         private $para = NULL;
         private $assunto = null;
         private $mensagem = null;
+        public $status = array( 'codigo_status' => null, 'descricao_status' => '' );
 
         public function __get($atributo){
             return $this->$atributo;
@@ -78,10 +79,64 @@
         $mail->isHTML(true);                                  //Set email format to HTML
         $mail->Subject = $mensagem->__get('assunto');
         $mail->Body    = $mensagem->__get('mensagem');
-        // $mail->AltBody = $mensagem->__get('mensagem');
+        $mail->AltBody = 'É necessario utilizar um client que suporte HTML para ter acesso total ao conteúdo dessa mensagem';
 
         $mail->send();
-        echo 'Message has been sent';
+        $mensagem->status['codigo_status'] = 1;
+        $mensagem->status['descricao_status'] = 'E-mail enviado com sucesso';
+
     } catch (Exception $e) {
-        echo "Não foi possível enviar o email, tente novamente mais tarde. Erro: {$mail->ErrorInfo}";
+        $mensagem->status['codigo_status'] = 2;
+        $mensagem->status['descricao_status'] = 'Não foi possivel enviar este e-mail! Por favor tente novamente mais tarde. Detalhes do erro: ' . $mail->ErrorInfo;    
     }
+
+
+
+    ?>
+
+<html>
+	<head>
+		<meta charset="utf-8" />
+    	<title>App Mail Send</title>
+
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	</head>
+
+	<body>
+
+		<div class="container">
+			<div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+				<h2>Send Mail</h2>
+				<p class="lead">Seu app de envio de e-mails particular!</p>
+			</div>
+
+			<div class="row">
+				<div class="col-md-12">
+
+					<?php if($mensagem->status['codigo_status'] == 1) { ?>
+
+						<div class="container">
+							<h1 class="display-4 text-success">Sucesso</h1>
+							<p><?= $mensagem->status['descricao_status'] ?></p>
+							<a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+						</div>
+
+					<?php } ?>
+
+					<?php if($mensagem->status['codigo_status'] == 2) { ?>
+
+						<div class="container">
+							<h1 class="display-4 text-danger">Ops!</h1>
+							<p><?= $mensagem->status['descricao_status'] ?></p>
+							<a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+						</div>
+
+					<?php } ?>
+
+				</div>
+			</div>
+		</div>
+
+	</body>
+</html>
