a7c26edc3d4d198fbb5ddf7b126aa3693b1a2df6
IagoMelo01 <iagomello160@gmail.com>
2023-09-11 20:19:49 -0300

    Envio da primeira mensagem usando smtp mailgun

--- processa_envio.php
+++ processa_envio.php
@@ -56,30 +56,29 @@
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+        $mail->Host       = 'smtp.mailgun.org';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'user@example.com';                     //SMTP username
-        $mail->Password   = 'secret';                               //SMTP password
+        $mail->Username   = 'postmaster@sandboxee48add78ce748cfb9ab6f6f584df931.mailgun.org';                     //SMTP username
+        $mail->Password   = 'fe97253c65268e03ef0efa731a37f9e5-413e373c-83390455';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         //Recipients
-        $mail->setFrom('from@example.com', 'Mailer');
-        $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-        $mail->addAddress('ellen@example.com');               //Name is optional
-        $mail->addReplyTo('info@example.com', 'Information');
-        $mail->addCC('cc@example.com');
-        $mail->addBCC('bcc@example.com');
+        $mail->setFrom('postmaster@sandboxee48add78ce748cfb9ab6f6f584df931.mailgun.org', 'Remetente');
+        $mail->addAddress('SendMailGitHubIM@proton.me', 'Destinatario');     //Add a recipient
+        // $mail->addReplyTo('info@example.com', 'Information');
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
 
         //Attachments
-        $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $mail->Subject = 'Teste Assunto';
+        $mail->Body    = 'Teste body <b>HTML!</b>';
+        $mail->AltBody = 'Teste body plain';
 
         $mail->send();
         echo 'Message has been sent';
