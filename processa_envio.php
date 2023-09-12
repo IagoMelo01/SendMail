3a02a7f5664ad93fdb1212080e01699e94aed3f3
Iago Melo <iagomello160@gmail.com>
2023-09-12 09:42:15 -0300

    Correção processa_envio.php

--- processa_envio.php
+++ processa_envio.php
@@ -57,23 +57,17 @@
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.mailgun.org';                     //Set the SMTP server to send through
+        $mail->Host       = 'SeuHost';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'postmaster@sandboxee48add78ce748cfb9ab6f6f584df931.mailgun.org';                     //SMTP username
-        $mail->Password   = 'fe97253c65268e03ef0efa731a37f9e5-413e373c-83390455';                               //SMTP password
+        $mail->Username   = 'SeuUserName';                     //SMTP username
+        $mail->Password   = 'SuaSenha';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         //Recipients
-        $mail->setFrom('postmaster@sandboxee48add78ce748cfb9ab6f6f584df931.mailgun.org');
+        $mail->setFrom('SeuEmissor');
         $mail->addAddress($mensagem->__get('para'));     //Add a recipient
-        // $mail->addReplyTo('info@example.com', 'Information');
-        // $mail->addCC('cc@example.com');
-        // $mail->addBCC('bcc@example.com');
-
-        //Attachments
-        // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+        
 
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
